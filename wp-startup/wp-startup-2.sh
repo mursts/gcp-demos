2ddd4b87ebc152861c42801f79922ef503cfe4a7
mursts <mursts@gmail.com>
2018-10-07 00:19:32 +0900

    ドキュメントルートを修正

--- wp-startup/wp-startup-2.sh
+++ wp-startup/wp-startup-2.sh
@@ -3,8 +3,7 @@ cd
 #以下、最新のWPのバージョンを指定する。
 curl -L -O https://ja.wordpress.org/wordpress-4.9.8-ja.zip
 unzip wordpress-*-ja.zip
-sudo mv wordpress/* /var/www
-sudo mv /var/www/index.html /var/www/index.html.bk
-sudo chgrp -R www-data /var/www
-sudo chmod -R g+w /var/www
-
+sudo mv wordpress/* /var/www/html
+sudo mv /var/www/html/index.html /var/www/html/index.html.bk
+sudo chgrp -R www-data /var/www/html
+sudo chmod -R g+w /var/www/html
